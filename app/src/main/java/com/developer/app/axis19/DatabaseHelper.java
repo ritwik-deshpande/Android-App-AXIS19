ebfcfa0de0253f4825c814093452e21168c6d386
chaityachheda <chaityachheda@gmail.com>
2019-01-23 21:25:20 +0530

    Single registration updated

--- app/src/main/java/com/developer/app/axis19/DatabaseHelper.java
+++ app/src/main/java/com/developer/app/axis19/DatabaseHelper.java
@@ -12,6 +12,8 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.developer.app.axis19.MainActivity.Email;
 import static com.developer.app.axis19.MainActivity.axisid;
@@ -38,37 +40,59 @@ public class DatabaseHelper {
         mPushDatabaseReference = mFirebaseDatabase.getReference().child("users");
         mPushDatabaseReference.child(key).setValue(user);
     }
-    public void registerUser (String category, String event_name) {
+    public void registerUser (final String category, final String event_name) {
 
         final DatabaseReference fromPath, toPath;
+        final Map<String,String> taskMap = new HashMap<>();
+
         ValueEventListener valueEventListener;
         key = utilFunctions.getUser_key(Email);
-        toPath = rootRef.child("Events").child(category).child(event_name).child("Registrations").child(key);
+        toPath = rootRef.child("eventRegistration").child(event_name).child(key).child(key);
         fromPath = rootRef.child("users").child(key);
-        toPath.setValue(axisid, new DatabaseReference.CompletionListener() {
+        valueEventListener = new ValueEventListener() {
             @Override
-            public void onComplete(DatabaseError firebaseError, DatabaseReference firebase) {
-                if (firebaseError != null) {
-                    System.out.println("Copy failed");
-                } else {
-                    System.out.println("Success"+axisid);
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //Toast.makeText(getContext(),"retrieving data",Toast.LENGTH_SHORT).show();
+                for(DataSnapshot ds : dataSnapshot.getChildren()) {
 
+                    taskMap.put("username", dataSnapshot.child("username").getValue(String.class));
+                    taskMap.put("phone",dataSnapshot.child("phone").getValue(String.class) );
+                    taskMap.put("axisid", axisid);
+                    taskMap.put("email", Email);
+                    taskMap.put("college", dataSnapshot.child("college").getValue(String.class));
                 }
+
+                toPath.setValue(taskMap, new DatabaseReference.CompletionListener() {
+                    @Override
+                    public void onComplete(DatabaseError firebaseError, DatabaseReference firebase) {
+                        if (firebaseError != null) {
+                            System.out.println("Copy failed");
+                        } else {
+                            System.out.println("Success"+axisid);
+
+                        }
+                    }
+                });
+
+                fromPath.child(category).child(event_name).setValue("Registered", new DatabaseReference.CompletionListener() {
+                    @Override
+                    public void onComplete(DatabaseError firebaseError, DatabaseReference firebase) {
+                        if (firebaseError != null) {
+                            System.out.println("Copy failed");
+                        } else {
+                            System.out.println("Success"+ axisid+"$");
+
+                        }
+                    }
+                });
+
+
             }
-        });
 
-
-        fromPath.child(category).child(event_name).setValue("Registered", new DatabaseReference.CompletionListener() {
             @Override
-            public void onComplete(DatabaseError firebaseError, DatabaseReference firebase) {
-                if (firebaseError != null) {
-                    System.out.println("Copy failed");
-                } else {
-                    System.out.println("Success"+ axisid+"$");
-
-                }
-            }
-        });
+            public void onCancelled(DatabaseError databaseError) {}
+        };
+        fromPath.addListenerForSingleValueEvent(valueEventListener);
 
     }
 
